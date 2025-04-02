655d76fbe3e8a0214a5973c164453bc4324bfbe9
Léon <leon.victor@augmenta.tech>
2025-04-02 11:03:05 +0200

    feat: Update example to latest API and improve it a bit

--- examples/Example.cpp
+++ examples/Example.cpp
@@ -1,9 +1,16 @@
-#include "../include/AugmentaServerLib.hpp"
+#include "../include/AugmentaClientSDK.hpp"
 
 #include <vector>
 #include <array>
+#include <iostream>
 
-using CustomPointCloudDataStructure = std::vector<std::array<float, 3>>;
+// Sample data types. Replace with your own types !
+using Vector3f = std::array<float, 3>;
+using PointCloud = std::vector<Vector3f>;
+struct Cluster {
+    Vector3f position;
+};
+using String = std::string;
 
 struct ExampleWebSocketClient
 {
@@ -17,11 +24,11 @@ struct ExampleWebSocketClient
         options.usePolling = false;
         // Specify the options you need
 
-        augmentaClient.initialize("my name", options); 
+        augmentaClient.initialize("my name", options);
     }
 
     // 2 -----
-    // When the client is notified that a connection was opened, we can send a registry request
+    // When the client is notified that a connection was opened, we send a registry request
     void OnConnectionOpened()
     {
         // Use the lib to generate the message according to your options
@@ -31,79 +38,124 @@ struct ExampleWebSocketClient
 
     // 3 ------
     //  The lib can be used to parse a data blob or messages to a more developer-friendly format
-    void OnMessageReceived(const std::string& message)
+    void OnMessageReceived(const std::string &message)
     {
-        const auto& parsedMessage = augmentaClient.parseControlMessage(message.c_str());
-    
+        const auto &parsedMessage = augmentaClient.parseControlMessage(message.c_str());
+
         if (parsedMessage.isSetup())
         {
-            if (parsedMessage.getStatus() == Augmenta::ControlMessage::Status::Ok)
+            if (parsedMessage.getStatus() == Augmenta::ControlMessage::Status::Error)
             {
-                // Handshake successful !
+                std::cout << "An error occured: " << parsedMessage.getErrorMessage() << std::endl;
+                return;
             }
+
+            const auto &world = parsedMessage.getRootObject();
+            TraverseObjectHierarchy(&world);
         }
         else if (parsedMessage.isUpdate())
         {
-            // Handle updates as necessary
+            // Same thing on update, save the data you need !
+            TraverseObjectHierarchy(&parsedMessage.getRootObject());
         }
     }
 
     // 4 (cont.) ------
-    void OnDataBlobReceived(const std::vector<std::byte>& dataBlob)
+    void OnDataBlobReceived(const std::vector<std::byte> &dataBlob)
     {
         auto parsedData = augmentaClient.parseDataBlob(dataBlob.data(), dataBlob.size());
-    
+
         // Scene info
-        auto& sceneInfo = parsedData.getSceneInfo();
-        // Do something with the data
-    
+        const auto &sceneInfo = parsedData.getSceneInfo();
+        
+        String scenePath;
+        scenePath.resize(sceneInfo.getAddressLength());
+        sceneInfo.getAddress(scenePath.data());
+
         // Objects
-        for (auto& object : parsedData.getObjects())
+        for (auto &object : parsedData.getObjects())
         {
+            const auto &objectID = object.getID();
             if (object.hasCluster())
             {
-                auto& clusterInfo = object.getCluster();
-                // Do something with the data
+                auto &clusterInfo = object.getCluster();
+
+                if (clusterInfo.getState() == Augmenta::ClusterState::Entered)
+                {
+                    // This is a new cluster, add it to our list
+                }
+
+                if (clusterInfo.getState() == Augmenta::ClusterState::WillLeave)
+                {
+                    // Clean up leaving clusters
+                }
             }
-    
+
             if (object.hasPointCloud())
             {
-                auto& pcInfo = object.getPointCloud();
+                auto &pcInfo = object.getPointCloud();
+
                 // Do something with the data
-                
                 // For example you can copy the point data to your own data structure.
-                CustomPointCloudDataStructure pc;
+                PointCloud pc;
                 pc.resize(pcInfo.getPointCount());
                 pcInfo.getPointsData(pc.data());
             }
         }
-    
+
         // Zones
-        for (const auto& zone : parsedData.getZones())
+        for (const auto &zone : parsedData.getZones())
         {
-            for (const auto& property : zone.getProperties())
+            for (const auto &property : zone.getProperties())
             {
-                switch(property.getType())
+                switch (property.getType())
                 {
-                    case Augmenta::DataBlob::ZonePacket::Property::Type::Slider:
-                    {
-                        const auto& sliderData = property.getExtraData<Augmenta::DataBlob::ZonePacket::SliderProperty>();
-                        auto value = sliderData->getValue();
-                    }
-                    case Augmenta::DataBlob::ZonePacket::Property::Type::XYPad:
-                    {
-                        const auto& xyData = property.getExtraData<Augmenta::DataBlob::ZonePacket::XYPadProperty>();
-                        auto x = xyData->getX();
-                        auto y = xyData->getY();
-                    }
+                case Augmenta::DataBlob::ZonePacket::Property::Type::Slider:
+                {
+                    const auto &sliderData = property.getSliderParameters();
+                    auto value = sliderData->getValue();
+                }
+                case Augmenta::DataBlob::ZonePacket::Property::Type::XYPad:
+                {
+                    const auto &xyData = property.getXYPadParameters();
+                    auto x = xyData->getX();
+                    auto y = xyData->getY();
+                }
                     // ...
                 }
             }
         }
     }
 
-    void SendMessageToServer(const std::string& message)
+    void SendMessageToServer(const std::string &message)
     {
         // ... do your thing
+    }
+
+    // Helper: Recursively traverse the object hierarchy (received on setup or update) and get the data you need
+    void TraverseObjectHierarchy(const Augmenta::ControlMessage::Container *container)
+    {
+        if (container->isScene())
+        {
+            const auto &sceneParameters = container->getSceneParameters();
+            const auto &sceneSize = sceneParameters->size;
+            // Maybe save that somewhere ? 
+        }
+
+        if (container->isZone())
+        {
+            const auto &zoneParameters = container->getZoneParameters();
+            if (zoneParameters->isBox())
+            {
+                const auto &boxParameters = zoneParameters->getBoxShapeParameters();
+                boxParameters->size;
+                // You might want to do something with this ?
+            }
+        }
+
+        for (const auto &child : container->getChildren())
+        {
+            TraverseObjectHierarchy(&child);
+        }
     }
 };
